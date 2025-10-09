aa6650d587b7b2e1d3e733c683f4b856aaca417c
Rouven Wachhaus (HBDI) <rouven.wachhaus@datenschutz.hessen.de>
2025-10-09 09:22:03 +0200

    fixes to language handling and improvements to configuration

--- matches.py
+++ matches.py
@@ -3,16 +3,30 @@ import re
 from dataclasses import dataclass, field
 import gettext
 import os
+import json
 
-lstr: str = "de"
-lenv = os.environ["LANGUAGE"]
+# configure support match types
+with open("config_types.json") as f:
+    config = json.load(f)
 
-if lenv in ["de", "en"]:
-    lstr = lenv
+config_ainer = config["ai-ner"]
+config_regex = config["regex"]
 
-lang = gettext.translation("base", localedir="locales", languages=[lstr])
-lang.install()
-_ = lang.gettext
+config_regex_sorted: dict[str, dict] = {}
+
+""" Since we use a concatenated regex that contains multiple types of checks (e. g. bank account, email all in one)
+    for efficiency reasons, we now have to figure out exactly which type of match it actually is. This is only possible
+    by looking at the position of the group storing the match within the overall regex.
+    Sort the regex configuration by position accordingly. """
+
+for conf in config_regex:
+    config_regex_sorted[conf["regex_compiled_pos"]] = conf
+
+""" And sort the AI NER configuration by matching terms. """
+config_ainer_sorted: dict[str, dict] = {}
+
+for conf in config_ainer:
+    config_ainer_sorted[conf["term"]] = conf
 
 """ Class for holding a singular found PII match.
 
@@ -23,21 +37,8 @@ class PiiMatch:
     text: str
     # The file in which the represented PII was found
     file: str
-
-    class PiiMatchType(StrEnum):
-        REGEX_RVNR = _("Regex: Pension Insurance Fond ID")
-        REGEX_IBAN = _("Regex: IBAN")
-        REGEX_EMAIL = _("Regex: Email address")
-        REGEX_IPV4 = _("Regex: IPv4 address")
-        REGEX_WORDS = _("Regex: Signal words")
-        REGEX_PGPPRV = _("Regex: private pgp key")
-        NER_PERSON = _("AI-NER: Person")
-        NER_LOCATION = _("AI-NER: Location")
-        NER_HEALTH = _("AI-NER: Health Data (experimental)")
-        NER_PASSWORD = _("AI-NER: Passwords (experimental)")
-
     # The type of PII found. Only types explicitly supported by this application are valid.
-    type: PiiMatchType
+    type: str
     # Only for PII found via AI-assisted NER: The likelihood with which a PII string represents a specific PII type, self-assessed by the model used.
     ner_score: float | None = None
 
@@ -52,9 +53,20 @@ class PiiMatchContainer:
     # Whitelist used for excluding strings from being identified as PII
     whitelist: list[str] = field(default_factory=list)
 
+    def by_file(self):
+        results: dict[str, list[PiiMatch]] = {}
+
+        for pm in self.pii_matches:
+            if pm.file in results.keys():
+                results[pm.file].append(pm)
+            else:
+                results[pm.file] = [pm]
+
+        return results
+
     """ Helper function for adding matches to the matches container. This generic, internal method is
         called by the other methods intended for public use, its aim is to reduce redundancy. """
-    def __add_match(self, text: str, file: str, type: PiiMatch.PiiMatchType, ner_score: float | None = None) -> None:
+    def __add_match(self, text: str, file: str, type: str, ner_score: float | None = None) -> None:
             whitelisted: bool = False
 
             # TODO: performance could be improved by creating a regex from the whitelist entries and matching against that.
@@ -63,7 +75,7 @@ class PiiMatchContainer:
                 if word in text:
                     whitelisted = True
                     break;
-            
+
             if whitelisted == False:
                 pm: PiiMatch = PiiMatch(text=text, file=file, type=type, ner_score=ner_score)
                 self.pii_matches.append(pm)
@@ -71,23 +83,11 @@ class PiiMatchContainer:
     """ Helper function for adding regex-based matches to the matches container. """
     def add_matches_regex(self, matches: re.Match | None, path: str) -> None:
         if matches is not None:
-            type: PiiMatch.PiiMatchType | None = None
+            type: str | None = None
 
-            """ Since we use a concatenated regex that contains multiple types of checks (e. g. bank account, email all in one)
-                for efficiency reasons, we now have to figure out exactly which type of match it actually is. This is only possible
-                by looking at the position of the group storing the match within the overall regex. """
-            if matches.groups()[0] is not None:
-                type = PiiMatch.PiiMatchType.REGEX_RVNR
-            elif matches.groups()[1] is not None:
-                type = PiiMatch.PiiMatchType.REGEX_IBAN
-            elif matches.groups()[2] is not None:
-                type = PiiMatch.PiiMatchType.REGEX_EMAIL
-            elif matches.groups()[3] is not None:
-                type = PiiMatch.PiiMatchType.REGEX_IPV4
-            elif matches.groups()[4] is not None:
-                type = PiiMatch.PiiMatchType.REGEX_WORDS
-            elif matches.groups()[5] is not None:
-                type = PiiMatch.PiiMatchType.REGEX_PGPPRV
+            for idx, item in enumerate(matches.groups()):
+                if item is not None:
+                    type = config_regex_sorted[idx]["label"]
 
             self.__add_match(text=matches.group(), file=path, type=type)
 
@@ -98,13 +98,6 @@ class PiiMatchContainer:
             for match in matches:
                 type: PiiMatch.PiiMatchType | None = None
 
-                if match["label"] == "Person's Name":
-                    type = PiiMatch.PiiMatchType.NER_PERSON
-                elif match["label"] == "Location":
-                    type = PiiMatch.PiiMatchType.NER_LOCATION
-                elif match["label"] == "Health Data":
-                    type = PiiMatch.PiiMatchType.NER_HEALTH
-                elif match["label"] == "Password":
-                    type = PiiMatch.PiiMatchType.NER_PASSWORD
+                type = config_ainer_sorted[match["label"]]["label"]
 
                 self.__add_match(text=match["text"], file=path, type=type, ner_score=match["score"])
