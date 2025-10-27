5c01599fd883323936945defedc7214b74aea848
Rouven Wachhaus (HBDI) <rouven.wachhaus@datenschutz.hessen.de>
2025-10-27 16:25:10 +0100

    fix merge commit

--- main.py
+++ main.py
@@ -14,20 +14,9 @@ from bs4 import BeautifulSoup
 from gliner import GLiNER
 import globals
 from matches import PiiMatchContainer
-<<<<<<< HEAD
-import gettext
-import json
-
-lstr: str = os.environ.get("LANGUAGE")
-lenv = lstr if lstr and lstr in ["de", "en"] else "de"
-
-lang = gettext.translation("base", localedir="locales", languages=[lenv])
-lang.install()
-_ = lang.gettext
-=======
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
->>>>>>> master
+
 
 """ Used to count how many files per extension have been found. This does *not* only count supported/qualified
     extensions but all of the ones contained in the root directory searched.
@@ -78,20 +67,6 @@ if not globals.args.ner and not globals.args.regex:
 
 if globals.args.ner == True:
     model: GLiNER = GLiNER.from_pretrained("urchade/gliner_medium-v2.1")
-<<<<<<< HEAD
-
-
-    #ner_labels = ["Person's Name", "Location", "Health Data", "Password"]
-    import json
-    with open("config_types.json") as f:
-        config = json.load(f)
-
-
-    ner_labels = [c["term"] for c in config["ai-ner"]]
-
-
-=======
->>>>>>> master
 
     import json
     with open("config_types.json") as f:
@@ -116,19 +91,10 @@ if globals.args.regex == True:
 else:
     globals.logger.info(globals._("Regex-based search is *not* active."))
 
-<<<<<<< HEAD
-    if args.ner == True:
-        file_log.write(_("AI-based search is active.\n"))
-    else:
-        file_log.write(_("AI-based search is *not* active.\n"))
-
-    file_log.write("\n\n")
-=======
 if globals.args.ner == True:
     globals.logger.info(globals._("AI-based search is active."))
 else:
     globals.logger.info(globals._("AI-based search is *not* active."))
->>>>>>> master
 
 globals.logger.info("\n")
 
@@ -212,54 +178,14 @@ for root, dirs, files in os.walk(globals.args.path):
             """ For HTML files, we use BeautifulSoup4 to extract the text without markup. """
             with open(full_path) as doc:
                 try:
-<<<<<<< HEAD
-                    # extract text page by page since that's not too memory-intensive
-                    for page_layout in extract_pages(full_path):
-                        for text_container in page_layout:
-                            if isinstance(text_container, LTTextContainer):
-                                text: str = text_container.get_text()
-
-                                """ Workaround for PDFs with messed-up text embeddings that only
-                                    consist of very short character sequences """
-                                if len(text) < 10:
-                                    continue
-                                else:
-                                    if args.regex == True:
-                                        matches: re.Match = regex_all.search(text)
-                                        pmc.add_matches_regex(matches, full_path)
-
-                                    if args.ner == True:
-                                        entities = model.predict_entities(text, ner_labels, threshold=0.5)
-                                        pmc.add_matches_ner(entities, full_path)
-
-                    num_files_checked += 1
-                except Exception as excpt:
-                    add_error(str(excpt), full_path)
-            elif ext == ".docx":
-                """ For DOCX files, we extract text using python-docx. Currently, this only takes a document's
-                    paragraphs into account, with no regard for elements that aren't paragraphs (headers, footers, tables)."""
-                try:
-                    doc: docx.Document = docx.Document(full_path)
-=======
                     soup: BeautifulSoup = BeautifulSoup(doc, "html.parser")
->>>>>>> master
                     num_files_checked += 1
 
                     text: str = soup.get_text()
 
-<<<<<<< HEAD
-                    paragraph: docx.text.paragraph
-                    for paragraph in doc.paragraphs:
-                        text += paragraph.text
-
-                        if args.regex == True:
-                            matches: re.Match = regex_all.search(text)
-                            pmc.add_matches_regex(matches, full_path)
-=======
                     if globals.args.regex == True:
                         matches: re.Match = regex_all.search(text)
                         pmc.add_matches_regex(matches, full_path)
->>>>>>> master
 
                     if globals.args.ner == True:
                         entities = model.predict_entities(text, ner_labels, threshold=0.5)
@@ -281,39 +207,7 @@ for root, dirs, files in os.walk(globals.args.path):
                 except Exception as excpt:
                     add_error(str(excpt), full_path)
 
-<<<<<<< HEAD
-                        text: str = soup.get_text()
-
-                        if args.regex == True:
-                            matches: re.Match = regex_all.search(text)
-                            pmc.add_matches_regex(matches, full_path)
-
-                        if args.ner == True:
-                            entities = model.predict_entities(text, ner_labels, threshold=0.5)
-                            pmc.add_matches_ner(entities, full_path)
-                    except UnicodeDecodeError:
-                        add_error("HTML Unicode Decode Error", full_path)
-            elif (ext == ".txt" or ext == "") and mimetypes.guess_type(full_path) == "text/plain":
-                with open(full_path) as doc:
-                    try:
-                        text: str = doc.read()
-
-                        if args.regex == True:
-                            matches: re.Match = regex_all.search(text)
-                            pmc.add_matches_regex(matches, full_path)
-
-                        if args.ner == True:
-                            entities = model.predict_entities(text, ner_labels, threshold=0.5)
-                            pmc.add_matches_ner(entities, full_path)
-                    except Exception as excpt:
-                        add_error(str(excpt), full_path)
-
-            if args.stop_count and num_files_all == args.stop_count:
-                break
-        if args.stop_count and num_files_all == args.stop_count:
-=======
         if globals.args.stop_count and num_files_all == globals.args.stop_count:
->>>>>>> master
             break
     if globals.args.stop_count and num_files_all == globals.args.stop_count:
         break
@@ -339,19 +233,7 @@ for k, v in errors.items():
     for f in v:
         globals.logger.info("\t\t{}".format(f.encode("utf-8", "replace")))
 
-<<<<<<< HEAD
-    file_log.write("\n\n")
-    file_log.write(_("Analysis finished at {}\n").format(time_end))
-    file_log.write(_("Performance of analysis: {} analyzed files per second\n").format(round(num_files_checked / max(time_diff.seconds, 1), 2)))
-
-with open("./output/" + outslug + "_findings.csv", "w") as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["match", "file", "type", "ner_score"])
-
-    for pm in pmc.pii_matches:
-        csvwriter.writerow([pm.text, pm.file, pm.type, pm.ner_score])
-=======
 globals.logger.info("\n")
 globals.logger.info(globals._("Analysis finished at {}").format(time_end))
 globals.logger.info(globals._("Performance of analysis: {} analyzed files per second").format(round(num_files_checked / max(time_diff.seconds, 1), 2)))
->>>>>>> master
+
